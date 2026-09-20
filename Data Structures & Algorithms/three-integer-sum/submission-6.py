e529b164cbc9e2de5bf0583f7901a382da6dad38
HueyAmiNhvtim <dshk127mm@gmail.com>
2026-09-20 17:58:42 -0400

    Add: three-integer-sum - submission-6

---
+++ Data Structures & Algorithms/three-integer-sum/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums = sorted(nums)
+        print(nums)
+        result = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            target = 0 - nums[i]
+            result += self.twoSum(nums, target, i+1, nums[i])
+
+        return result
+
+    def twoSum(self, nums: List[int], target: int, start: int, original: int):
+        result = []
+        l = start
+        r = len(nums) - 1
+        while l < r:
+            two_sum = nums[l] + nums[r]
+            if two_sum == target:
+                result.append([original, nums[l], nums[r]])
+                l += 1
+                r -= 1
+                while nums[l] == nums[l-1] and l < r:
+                    l += 1
+            elif two_sum > target: # Too big, move the r pointer back
+                r -= 1
+            else: # Too small, move l forward
+                l += 1
+        return result            
+
